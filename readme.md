9271e4c81b6cd8a5dd5f1149cc3d9ce6dba1ad05
Tculls <tanner.culley@gmail.com>
2022-03-25 13:43:08 -0700

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Practice Assignment for Web Fundamentals JavaScript Section: Language Fundamentals
---
+++ script.js
@@ -0,0 +1,28 @@
+// create a function [x]
+// create some variables [x]
+// special command for multipls of 3,5 and 3and5 [x]
+// return array 
+
+function count(){
+    for(let i=0; i <=100; i++){
+      if(i % 3 ==0 && i % 5 ==0){
+        console.log("FizzBuzz")
+      } 
+      else if(i % 3 == 0){
+        console.log("Fizz");
+      }
+      else if(i % 5 == 0){
+        console.log("Buzz")
+      }   
+      console.log(i)
+    }
+  }
+  count()
+
+
+
+// else(i % 3 && i % 5){
+            // console.log("Fizz"+"Buzz")
+            // }
+            // Else is default 
+            // and redundant 
